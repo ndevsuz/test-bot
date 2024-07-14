22177a783aa8be5a8c290b43879302c1959be48b
Iskandar Kodirov <eiscocyber@gmail.com>
2024-07-15 00:05:05 +0500

    Repos wrote

---
+++ src/TestBot.Console/Repositories/ITestRepository.cs
@@ -0,0 +1,14 @@
+using System.Linq.Expressions;
+using TestBot.Console.Models;
+
+namespace TestBot.Console.Repositories;
+
+public interface ITestRepository
+{
+    Task<Test> AddAsync(Test entity);
+    Task<Test> UpdateAsync(Test entity);
+    Task<bool> DeleteAsync(Expression<Func<Test, bool>> expression);
+    Task<Test> SelectAsync(Expression<Func<Test, bool>> expression, string[] includes = null);
+    IQueryable<Test> SelectAll(Expression<Func<Test, bool>> expression = null, string[] includes = null, bool isTracking = true);
+    Task<int> SaveAsync();
+}
---
+++ src/TestBot.Console/Repositories/TestRepository.cs
@@ -0,0 +1,66 @@
+using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using TestBot.Console.Contexts;
+using TestBot.Console.Models;
+
+namespace TestBot.Console.Repositories;
+
+public class TestRepository : ITestRepository
+{
+    private readonly DbSet<Test> table;
+    private readonly AppDbContext dbContext;
+
+    public TestRepository(AppDbContext dbContext)
+    {
+        this.dbContext = dbContext;
+    }
+
+    public async Task<Test> AddAsync(Test entity)
+    {
+        await table.AddAsync(entity);
+
+        return entity;
+    }
+
+    public async Task<Test> UpdateAsync(Test entity)
+    {
+        EntityEntry<Test> entry = this.dbContext.Update(entity);
+
+        return entry.Entity;
+    }
+
+    public async Task<bool> DeleteAsync(Expression<Func<Test, bool>> expression)
+    {
+        var entity = await this.SelectAsync(expression);
+
+        if (entity is not null)
+        {
+            table.Remove(entity);
+            return true;
+        }
+
+        return false;
+    }
+
+    public async Task<Test> SelectAsync(Expression<Func<Test, bool>> expression, string[] includes = null)
+        => await this.SelectAll(expression, includes).FirstOrDefaultAsync();
+
+    public IQueryable<Test> SelectAll(Expression<Func<Test, bool>> expression = null, string[] includes = null, bool isTracking = true)
+    {
+        var query = expression is null ? isTracking ? table : table.AsNoTracking()
+            : isTracking ? table.Where(expression) : table.Where(expression).AsNoTracking();
+
+        if (includes is not null)
+            foreach (var include in includes)
+                query = query.Include(include);
+
+        return query;
+    }
+    
+    public async Task<int> SaveAsync()
+    {
+        var result = await dbContext.SaveChangesAsync();
+        return result;
+    }
+}
